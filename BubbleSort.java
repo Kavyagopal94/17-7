dc89d09a18143ee61009d36d58fcf206a89c51fb
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-07-18 14:39:13 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+
+    static void Sort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9,2,7,5,8,6,4,1,3};
+        Sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
